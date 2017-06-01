c434fde378f8768fcef7952fea17ba488ece18b9
JamesVaughan <James.Vaughan@utoronto.ca>
2017-06-01 14:46:01 -0400

    Initial commit with VS solution with a submodule for XTMF2.

---
+++ TMG-Framework/TMG-Framework/Data/SparseMatrix.cs
@@ -0,0 +1,39 @@
+﻿/*
+    Copyright 2017 University of Toronto
+
+    This file is part of TMG-Framework for XTMF2.
+
+    TMG-Framework for XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TMG-Framework for XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TMG-Framework for XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMG.Data
+{
+    public sealed class SparseMatrix<T>
+    {
+        public SparseMap Map { get; private set; }
+        public T[] Data { get; private set; }
+
+        private int RowLength;
+
+        public SparseMatrix(SparseMap map)
+        {
+            Map = map;
+            RowLength = Map.Count;
+            Data = new T[Map.Count * Map.Count];
+        }
+    }
+}
