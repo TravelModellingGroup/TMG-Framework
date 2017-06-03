8eb8e9945cf9d749c757365937387b3af98625ee
JamesVaughan <James.Vaughan@utoronto.ca>
2017-06-03 09:31:06 -0400

    Added project with unit testing for the framework and a single unit test.

---
+++ TMG-Framework/TMG-Framework.Test/EnsureRuntimeLaunch.cs
@@ -0,0 +1,36 @@
+/*
+    Copyright 2017 University of Toronto
+
+    This file is part of TMG-Framework for XTMF2.
+
+    TMG-Framework for XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TMG-Framework for XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TMG-Framework for XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XTMF2;
+using System.Reflection;
+
+namespace TMG.Test
+{
+    [TestClass]
+    public class EnsureRuntimeLaunch
+    {
+        [TestMethod]
+        public void LaunchRuntime()
+        {
+            var runtime = XTMFRuntime.CreateRuntime();
+            runtime.SystemConfiguration.LoadAssembly(typeof(Range).GetTypeInfo().Assembly.Location);
+        }
+    }
+}
