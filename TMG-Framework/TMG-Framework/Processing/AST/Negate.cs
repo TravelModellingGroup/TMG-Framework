291a00afa9030800377602b27d7a86ad9d950129
James Vaughan <phibred@hotmail.com>
2018-02-19 09:04:45 -0500

    Fixed license header.

--- TMG-Framework/TMG-Framework/Processing/AST/Negate.cs
+++ TMG-Framework/TMG-Framework/Processing/AST/Negate.cs
@@ -1,20 +1,20 @@
 ﻿/*
     Copyright 2018 Travel Modelling Group, Department of Civil Engineering, University of Toronto
 
-    This file is part of XTMF.
+    This file is part of XTMF2.
 
-    XTMF is free software: you can redistribute it and/or modify
+    XTMF2 is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    XTMF is distributed in the hope that it will be useful,
+    XTMF2 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+    along with XTMF2.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
