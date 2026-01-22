170b93329ce59ac56c1f42a62b4aefd6aa4ad973
James Vaughan <James.Vaughan@utoronto.ca>
2026-01-22 14:58:32 -0500

    Updated copyright header

--- TMG-Framework/src/TMG-Framework/Utilities/VectorHelper/Conditions/FlagAnd.cs
+++ TMG-Framework/src/TMG-Framework/Utilities/VectorHelper/Conditions/FlagAnd.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright 2015-2016 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+    Copyright 2015-2026 Travel Modelling Group, Department of Civil Engineering, University of Toronto
 
     This file is part of XTMF.
 
